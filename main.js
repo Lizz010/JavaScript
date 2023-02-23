f7abf4c35761282d987cdcc864013c47adaec45b
lizz <lizzp0110@gmail.com>
2023-02-22 23:20:41 -0500

    Practicing console.log

---
+++ main.js
@@ -0,0 +1,20 @@
+/*Write sentence console.log*/
+console.log("¡Hola Mundo!");
+/*Write your full name*/
+console.log("Liz Pemberthy Peters");
+/*Write your date of birth*/
+console.log("01/01/2100");
+/*Write your date of birth without qoutes*/
+console.log(01012100);
+/*Write your full name and your year of birth,
+Write the same thing on one line in separate elements*/
+console.log("Liz Pemberthy (2100)");
+console.log("Liz", "Pemberthy", "(2100)");
+/*Write your full name and year of birth in different lines*/
+console.log("Liz");
+console.log("Pemberthy");
+console.log("(2100)");
+/*Concatenate your full name and YB*/
+console.log("Liz" + "Pemberthy" + "(2100)");
+/*Concatenate your full name and YB with spaces*/
+console.log("Liz " + "Pemberthy " + "(2100)")
