d0e9a21fd23088a5d1bfbf1bba45b2451a3fca49
lizz <lizzp0110@gmail.com>
2023-02-24 22:50:06 -0500

    Variables

---
+++ Variables/variable2.js
@@ -0,0 +1,51 @@
+/*LABORATORIO
+Tiempo Estimado
+15-30 minutos
+Nivel de Dificultad
+Fácil
+Objetivos
+Familiarizar al estudiante con:
+variables (es decir, nombrar, declarar, inicializar y modificar su valor)
+Escenario
+Nuestra tarea será crear una lista de contactos. Inicialmente, la lista
+será bastante simple: solo escribiremos tres personas utilizando los
+datos que se muestran en la tabla a continuación. En el resto del curso,
+volverá a este script y lo ampliará sistemáticamente con nuevas
+funciones, utilizando los elementos de JavaScript recién aprendidos.
+
+Nombre	         Teléfono	      Correo
+Maxwell Wright	 (0191) 719 6495  Curabitur.egestas.nunc@nonummyac.co.uk
+Raja Villarreal	 0866 398 2895	  posuere.vulputate@sed.com
+Helen Richards	 0800 1111	      libero@convallis.edu
+
+Declara e inicializa las variables donde almacenarás toda la información
+(nueve variables en total). Muestra en la consola información sobre el
+primer y último contacto en el formulario: nombre/teléfono/correo.*/
+
+/*Declarando variables nombre*/
+let nombre = "Maxwell Wright";
+let nombre1 = "Raja Villarreal";
+let nombre2 = "Helen Richards";
+
+/*Declarando variables telefono*/
+let telefono = 01917196495;
+let telefono1 = 08663982895;
+let telefono2 = 08001111;
+
+/*Declarando variables email*/
+let email = "Curabitur.egestas.nunc@nonummyac.co.uk";
+let email1 = "posuere.vulputate@sed.com";
+let email2 = "libero@convallis.edu";
+
+/*Imprimiendo variables nombre, telefono, email*/
+console.log("Nombre: ", nombre);
+console.log("Nombre: ", nombre1);
+console.log("Nombre: ", nombre2);
+
+console.log("Telefono: ", telefono);
+console.log("Telefono: ", telefono1);
+console.log("Telefono: ", telefono2);
+
+console.log("Email: ", email);
+console.log("Email: ", email1);
+console.log("Email: ", email2);
