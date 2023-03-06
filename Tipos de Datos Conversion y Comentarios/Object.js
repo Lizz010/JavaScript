8b1b35242876c5f538d490bf2a626a8acba5b5a0
lizz <lizzp0110@gmail.com>
2023-03-05 20:45:22 -0500

    Objects

---
+++ Tipos de Datos Conversion y Comentarios/Object.js
@@ -0,0 +1,27 @@
+// OBJECTOS EN JAVASCRIPT
+let user1 = {
+    name: "Liz",
+    surname: "Pemberthy",
+    age: 100,
+    email: "lizpemberthy@teleworm.us"
+};
+
+let user2 = {
+    name: "Evan",
+    surname: "Peters",
+    age: 200,
+    email: "evanpeters@dayrep.com"
+};
+
+console.log(user1.name); // -> Liz
+console.log(user2.name); // -> Evan
+
+console.log(user1.age); // -> 100
+user1.age = 110;
+console.log(user1.age); // -> 110
+
+console.log(user2.phone); // -> undefined
+user2.phone = "904-399-7557";
+console.log(user2.phone); // -> 904-399-7557
+
+
