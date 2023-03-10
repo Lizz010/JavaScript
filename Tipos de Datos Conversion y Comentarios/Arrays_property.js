b5f9d5d92857cd71910012135182b3f370764373
lizz <lizzp0110@gmail.com>
2023-03-09 23:50:06 -0500

    Array property .length

--- Tipos de Datos Conversion y Comentarios/Arrays_property.js
+++ Tipos de Datos Conversion y Comentarios/Arrays_property.js
@@ -1,9 +1,14 @@
+// PROPERTY LENGTH
+
+// Imprimiendo longitud del array
 let names  = ["Evan", "Liz", "Asa", "Emma"];
 console.log(names.length); // -> 4
 
+// Insertando nuevo elemento en el array
 names[5] = "Ada";
 console.log(names.length); // -> 6
 
+// Imprimiendo resultados
 console.log(names); // -> ["Evan", "Liz", "Asa", "Emma", undefined, "Ada"]
 console.log(names[3]); // -> Emma
 console.log(names[4]); // -> undefined
