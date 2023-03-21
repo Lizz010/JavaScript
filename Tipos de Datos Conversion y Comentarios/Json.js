96d3d725eef1f26c1738f83f5290737285dacc55
lizz <lizzp0110@gmail.com>
2023-03-20 23:20:19 -0500

    Json

---
+++ Tipos de Datos Conversion y Comentarios/Json.js
@@ -0,0 +1,9 @@
+// JSON
+// Formato para el intercambio demdatos similares, y un subcunjunto
+// de los objetos
+
+var miObjecto = {		//Creando objecto
+	nombre : "Shakira"
+}
+// Convirtiendolo a Json con propidad .stringify
+console.log(JSON.stringify(miObjecto))
