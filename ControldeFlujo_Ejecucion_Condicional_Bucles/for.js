dbba6a478d0607e87f077c809a14f86e4cb7a901
lizz <lizzp0110@gmail.com>
2023-03-25 22:37:48 -0500

    Cycle for

---
+++ ControldeFlujo_Ejecucion_Condicional_Bucles/for.js
@@ -0,0 +1,74 @@
+// CYCLE FOR
+// Ejecutando ciclo for
+for (i = 0; i <= 10; i++) {
+    console.log("Hola" + i + "veces");
+}
+// Ejecutando ciclo for con condicionales --------------------
+for (i = 0; i <= 10; i++) {
+    if (i == 1) {
+        console.log("Hola" + i + "Vez");
+    } else {
+        console.log("Hola" + i + "Veces");
+    }
+}
+// Ejecutando ciclo for con arrays --------------------------
+var frutas = ["Manzana", "Sandia", "Naranja"];
+for (i = 0; i < frutas.length; i++) {
+    const frut = frutas.length[i];
+    console.log(frut);
+}
+// Ejecutando ciclo for con objetos -------------------------
+// Objetos
+var persona1 = {
+    codigo: 100
+    nombre: Evan,
+    profesion: Actor
+}
+var persona2 = {
+    codigo: 101
+    nombre: Maeve,
+    profesion: Escritora
+}
+var persona3 = {
+    codigo: 102
+    nombre: Cole,
+    profesion: Estudiante
+}
+// Llenando array con objetos
+var personas = [persona1, persona2, persona3];
+for (i = 0; personas.length; i++) {
+    const pers = personas[i].nombre; //Guardando nombres en variable pers
+    console.log(pers); //Imprimiendo pers
+}
+// Usando "break" para romper un ciclo for ----------------------
+var persona1 = {
+    codigo: 100
+    nombre: Evan,
+    profesion: Actor
+}
+var persona2 = {
+    codigo: 101
+    nombre: Maeve,
+    profesion: Escritora
+}
+var persona3 = {
+    codigo: 102
+    nombre: Cole,
+    profesion: Estudiante
+}
+// Llenando array con objetos
+var personas = [persona1, persona2, persona3];
+for (i = 0; personas.length; i++) {
+    if (personas[i].codigo > 100) {
+        break; //Rompiendo el ciclo si se cumple la condicion
+    }
+    const pers = personas[i].nombre; //Guardando nombres en variable pers
+    console.log(pers); //Imprimiendo pers
+}
+//Los forEach son especiales para los arrays
+//Usando forEach en arrays
+personas.forEach(pers => console.log(pers.profesion));
+//Usando forEach mostrando indices de arrays
+personas.forEach((pers, i) => {
+    console.log(i)
+})
