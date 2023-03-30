046f93835373629129c45ba4c876664eda23d36a
lizz <lizzp0110@gmail.com>
2023-03-29 23:49:08 -0500

    Laboratory

--- Tipos de Datos Conversion y Comentarios/Laboratory(Objects-Arrays).js
+++ Tipos de Datos Conversion y Comentarios/Laboratory(Objects-Arrays).js
@@ -14,29 +14,36 @@ elemento del cual será un objeto que describa a un solo usuario.
 Entonces, deberíamos tener un arreglo de tres elementos, y
 cada objeto colocado en el contendrá tres piezas de
 información (nombre, teléfono y correo electrónico).*/
-let contacts = [
-    {
-        name:"Maxwell Wright",
-        phone:"0191 719 6495",
-        email:"Curabitur.egestas.nunc@nonummyac.co.uk"
-    },{
-        name:"Raja Villarreal",
-        phone:"0866 398 2895",
-        email:"posuere.vulputate@sed.com"
-    },{
-        name:"Helen Richards",
-        phone:"0800 1111",
-        email:"libero@convallis.edu"
-    }];
-    
-    let newcont = {
-        name:"Maisie Haley",
-        phone:"0913 531 3030",
-        email:"risus.Quisque@urna.ca"
-    }
-    contacts.push(newcont);
-    console.log(contacts.length);
-    console.log(contacts[0].name);
-    console.log(contacts[4].name);
-    
-    
+let contacts = [{
+    name: "Maxwell Wright",
+    phone: "0191 719 6495",
+    email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+}, {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "posuere.vulputate@sed.com"
+}, {
+    name: "Helen Richards",
+    phone: "0800 1111",
+    email: "libero@convallis.edu"
+}];
+/*Al final de la lista declarada de esta manera, agrega un nuevo
+contacto usando el método del arreglo apropiado. El nuevo
+contacto es: Maisie Haley / 0913 531 3030 / risus.Quisque@urna.ca.*/
+let newcont = {
+    name: "Maisie Haley",
+    phone: "0913 531 3030",
+    email: "risus.Quisque@urna.ca"
+}
+/*Muestra el primer y último contacto, de nuevo en el formato: nombre /
+teléfono / correo electrónico. Utiliza la propiedad length del arreglo para
+determinar el índice del último elemento. Recuerde que los elementos del
+arreglo se indexan a partir de 0.*/
+contacts.push(newcont);
+console.log(contacts.length);
+console.log(contacts[0].name);
+console.log(contacts[0].phone);
+console.log(contacts[0].email);
+console.log(contacts[3].name);
+console.log(contacts[3].phone);
+console.log(contacts[3].email);
