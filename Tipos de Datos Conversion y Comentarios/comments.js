5190d414a1f287ada768ea609658ba94cfae3d36
Lizz010 <lizzp0110@gmail.com>
2023-05-15 23:57:52 -0500

    Comments.js

---
+++ Tipos de Datos Conversion y Comentarios/comments.js
@@ -0,0 +1,5 @@
+// Este es un comentario de una sola línea
+let name = "Lizz"; // Este también es un comentario de una sola línea, aunque la parte anterior a la doble diagonal es un código que se ejecutará.
+// Esta línea y la siguiente serán ignoradas
+// x = 8;
+console.log(name); // -> Lizz
