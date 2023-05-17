ef2cd823a859c724c45675e5f2fe65702169c027
Lizz010 <lizzp0110@gmail.com>
2023-05-16 23:57:36 -0500

    Comments.js

--- Tipos de Datos Conversion y Comentarios/comments.js
+++ Tipos de Datos Conversion y Comentarios/comments.js
@@ -3,3 +3,10 @@ let name = "Lizz"; // Este también es un comentario de una sola línea, aunque 
 // Esta línea y la siguiente serán ignoradas
 // x = 8;
 console.log(name); // -> Lizz
+
+// Comentarios de varias líneas
+// Los comentarios de varias líneas, también conocidos como comentarios de bloque,
+// permiten que los comentarios abarquen varias líneas, pero también te permiten colocar 
+// comentarios dentro de una línea, lo que no es posible con los comentarios de una sola línea.Se 
+// crean con una diagonal y un asterisco al principio del comentario y un asterisco y una diagonal 
+// al final.
