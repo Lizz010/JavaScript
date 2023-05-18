f92ae43fb5aa62839ccf6114cd2ae3696c3f3a04
Lizz010 <lizzp0110@gmail.com>
2023-05-17 23:51:03 -0500

    Comments js

--- Tipos de Datos Conversion y Comentarios/comments.js
+++ Tipos de Datos Conversion y Comentarios/comments.js
@@ -3,6 +3,7 @@ let name = "Lizz"; // Este también es un comentario de una sola línea, aunque 
 // Esta línea y la siguiente serán ignoradas
 // x = 8;
 console.log(name); // -> Lizz
+// ------------------------------------------------------------
 
 // Comentarios de varias líneas
 // Los comentarios de varias líneas, también conocidos como comentarios de bloque,
@@ -10,3 +11,63 @@ console.log(name); // -> Lizz
 // comentarios dentro de una línea, lo que no es posible con los comentarios de una sola línea.Se 
 // crean con una diagonal y un asterisco al principio del comentario y un asterisco y una diagonal 
 // al final.
+/*
+    Este es un bloque
+    de comentarios y puede
+    abarcar varias líneas
+
+    Entonces este código no se ejecutará
+    console.log("¡Hola, Mundo!");
+*/
+let y /* porque no hay mejor nombre */ = 100;
+console.log(y);
+// ------------------------------------------------------------
+
+// inicializar HelloText con Hola
+const HelloText = "Hola";
+
+angle = angle + 90; // girar 90 grados para compensar la pantalla vertical
+
+// comprobar que 0 no sea el divisor
+let result = a / b;
+
+// no hay necesidad de comprobar el divisor
+let result1 = a / b;
+
+// dividir a entre b
+let result2 = a / b;
+// ------------------------------------------------------------
+
+/*Tarea
+Hay un código que actualmente no funciona. Intenta arreglarlo usando solo comentarios. Intenta, si es posible,
+usar los atajos de teclado en tu editor para este propósito.*/
+
+"use strict"; 
+const prefix = "username_"; 
+let userName = "Jack"; 
+const userName = "Adam"; 
+let prefixedUserName; 
+const prefixedUserName; 
+userName = "John"; 
+prefixedUserName = prefix + userName; 
+ 
+console.log(prefixedUserName + prefixedUserName2); 
+console.log(prefixedUserName2); 
+
+// Solution
+
+"use strict"; 
+const prefix = "username_"; 
+let userName = "Jack"; 
+// const userName = "Adam"; 
+let prefixedUserName; 
+// const prefixedUserName; 
+userName = "John"; 
+prefixedUserName = prefix + userName; 
+ 
+console.log(prefixedUserName /*+ prefixedUserName2*/); 
+// console.log(prefixedUserName2); 
+
+
+
+
