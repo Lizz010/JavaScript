ea52b1383779dda1d8d7da0df9f605b201dbf423
Lizz010 <lizzp0110@gmail.com>
2023-06-08 23:44:40 -0500

    String operator

---
+++ Operadores/opStrings.js
@@ -0,0 +1,11 @@
+//String operator example
+let greetings = "Hi";
+console.log(greetings + " " + "Lizz"); // -> Hi Lizz
+
+let sentence = "Happy New Year ";
+let newSentence = sentence + 10000;
+
+console.log(newSentence); // -> Happy New Year 10000
+console.log(typeof newSentence); // -> string
+    
+
