b3c405ac53e57d494e889790b618d89deebd5663
Lizz010 <lizzp0110@gmail.com>
2023-06-09 23:43:14 -0500

    Compound Assignment Operators

---
+++ Operadores/opCompoundAssignment.js
@@ -0,0 +1,9 @@
+/*COMPOUNT ASSIGNMENT OPERATORS*/
+
+let sentence = "Happy New ";
+sentence += "Year ";
+sentence += 10000;
+console.log(sentence); // -> Happy New Year 10000
+
+//"sentences += "Year"" es igual que si se escribiera
+//"sentences = sentences + "Year"s"
