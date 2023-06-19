64b476f85b115c52861d99ddfdb148e36fef73ba
Lizz010 <lizzp0110@gmail.com>
2023-06-18 23:53:49 -0500

    Confirmation Dialogs

--- Interaccion_con_el_user/CuadrosDialogo.js
+++ Interaccion_con_el_user/CuadrosDialogo.js
@@ -1,3 +1,12 @@
 //CONFIRMATION DIALOGS 
 let decision = window.confirm("¿Está bien?");
 console.log(decision);
+
+//Another example
+let remove = confirm("¿Eliminar todos los datos?");
+let message = remove ? "Eliminando Datos" : "Cancelado"
+
+console.log(message);
+    
+
+
