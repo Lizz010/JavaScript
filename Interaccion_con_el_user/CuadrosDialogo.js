dd55eee4787bf7b76de274d0ce5f5668e270e5d2
Lizz010 <lizzp0110@gmail.com>
2023-06-19 23:49:14 -0500

    Prompt Dialogs

--- Interaccion_con_el_user/CuadrosDialogo.js
+++ Interaccion_con_el_user/CuadrosDialogo.js
@@ -7,6 +7,14 @@ let remove = confirm("¿Eliminar todos los datos?");
 let message = remove ? "Eliminando Datos" : "Cancelado"
 
 console.log(message);
+
+//PROMPT DIALOG
+let name = window.prompt("¿Cuál es tu nombre?", "");
+name = name ? name : "anónimo";
+
+let age = prompt("Hola " + name + " ¿Cuántos años tienes?");
+alert(name + " tiene " + age + " años");
+
     
 
 
