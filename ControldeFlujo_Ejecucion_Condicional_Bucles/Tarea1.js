7546a8503185ee28a13f0950f203742bd7e6e593
Lizz010 <lizzp0110@gmail.com>
2023-07-24 23:52:06 -0500

    using conditional if, if else

---
+++ ControldeFlujo_Ejecucion_Condicional_Bucles/Tarea1.js
@@ -0,0 +1,13 @@
+
+/*Tarea 1
+
+Escribe un script que le pida al usuario que ingrese un número. Muestra el mensaje "¡Bingo!"
+cuando el número sea mayor que 90 pero menor que 110, de lo contrario muestra el mensaje: "¡Fallaste!".
+Utiliza la sentencia if .*/
+
+let num = prompt('WRITE A NUMBER: ');
+if (num>90 && num<110){
+        alert('Bingooo!!');
+    } else{
+        alert('Fallaste!! :((');
+    }
