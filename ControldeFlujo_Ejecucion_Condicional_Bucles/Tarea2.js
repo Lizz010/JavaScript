ae9ef1e7e5649b4d259ce268a8f70482628d6a9c
Lizz010 <lizzp0110@gmail.com>
2023-07-27 22:41:11 -0500

    Ternary operator, if statement

--- ControldeFlujo_Ejecucion_Condicional_Bucles/Tarea2.js
+++ ControldeFlujo_Ejecucion_Condicional_Bucles/Tarea2.js
@@ -1,5 +1,11 @@
 /*Tarea 2
 Reescribe el código anterior, reemplazando el if con un operador condicional ternario.*/
 
-let num= prompt('Write a number: ');
-if (num>90 && num<110 ? alert('Bingooo!!!'): alert('Fallaste!! :(('));
+let num = prompt('Write a number: ');
+if (num > 90 && num < 110 ? alert('Bingooo!!!') : alert('Fallaste!! :(('));
+
+/**Other Solution */
+let number = prompt("Introduce un número aleatorio: ");
+let message = (number > 90 && number < 110) ? "¡Bingo!" : "¡Fallaste!";
+alert(message);
+
