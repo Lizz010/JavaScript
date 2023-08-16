b6746e7991d4ece78e3971a15973fb786a52f0b7
Lizz010 <lizzp0110@gmail.com>
2023-08-15 23:49:49 -0500

    Tarea 1 cycle for

--- ControldeFlujo_Ejecucion_Condicional_Bucles/Tarea1_cycle_for.js
+++ ControldeFlujo_Ejecucion_Condicional_Bucles/Tarea1_cycle_for.js
@@ -1,9 +1,9 @@
 // BUCLES - CYCLES
+
 // Tarea 1
 // Escribe un fragmento de código que escriba números del 100 al 0 en la consola,
 // pero en pasos de 10 en 10; entonces sería 100, 90, 80... etc.
 
-
-for (i = 100; i>=0; i-=10){
+for (i = 100; i >= 0; i -= 10) {
     console.log(i)
 }
